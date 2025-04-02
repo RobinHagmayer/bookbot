749d77a9597a7382d37672edad6100d9c723f141
RobinHagmayer <robin.hagmayer@proton.me>
2025-04-02 14:31:59 +0200

    refactor: comply to boot.dev output and function placement

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from stats import get_num_words
-
-
-def count_characters(content: str) -> dict[str, int]:
-    counted_chars: dict[str, int] = {}
-    for char in content.strip():
-        if not char.isalpha():
-            continue
-        char = char.lower()
-        if char not in counted_chars:
-            counted_chars[char] = 1
-            continue
-        counted_chars[char] += 1
-
-    return counted_chars
+from stats import count_characters, get_num_words
 
 
 def main():
@@ -22,17 +8,19 @@ def main():
         file_contents = f.read()
 
     num_words = get_num_words(file_contents)
-    print(f"--- Begin report of {file_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     counted_characters = count_characters(file_contents)
     sorted_characters = dict(
         sorted(counted_characters.items(), key=lambda item: item[1], reverse=True)
     )
     for key, value in sorted_characters.items():
-        print(f"The '{key}' character was found {value} times")
-
-    print("--- End report ---")
+        print(f"{key}: {value}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def get_num_words(content: str) -> int:
     words = content.split()
     return len(words)
+
+
+def count_characters(content: str) -> dict[str, int]:
+    counted_chars: dict[str, int] = {}
+    for char in content.strip():
+        if not char.isalpha():
+            continue
+        char = char.lower()
+        if char not in counted_chars:
+            counted_chars[char] = 1
+            continue
+        counted_chars[char] += 1
+
+    return counted_chars
